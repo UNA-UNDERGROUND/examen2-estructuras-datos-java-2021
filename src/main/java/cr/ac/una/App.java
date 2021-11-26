9b8fd23eeba1c867bc4f01770177a588c519e7e1
Jonathan Guzmán <jonguz5@hotmail.com>
2021-11-25 21:08:03 -0600

    imprimir triangulo de pascar

--- src/main/java/cr/ac/una/App.java
+++ src/main/java/cr/ac/una/App.java
@@ -11,13 +11,13 @@ public final class App {
 
     public static void main(String[] args) {
         // ejercicio 1
-        System.out.println("Ejercicio 1");
-        new Ejercicio1().run();
+        //System.out.println("Ejercicio 1");
+        //new Ejercicio1().run();
         System.out.println("Ejercicio 2");
         new Ejercicio2().run();
-        System.out.println("Ejercicio 3");
-        new Ejercicio3().run();
-        System.out.println("Ejercicio 5");
-        new Ejercicio5().run();
+        //System.out.println("Ejercicio 3");
+        //new Ejercicio3().run();
+        //System.out.println("Ejercicio 5");
+        //new Ejercicio5().run();
     }
 }
--- src/main/java/cr/ac/una/ejercicios/Ejercicio2.java
+++ src/main/java/cr/ac/una/ejercicios/Ejercicio2.java
@@ -1,9 +1,11 @@
 package cr.ac.una.ejercicios;
 
+import java.util.List;
+
 public class Ejercicio2 {
 
     /**
-     * genera las filas del triangulo de pascal
+     * genera las filas del triangulo de pascal de manera recursiva
      * 
      * EJ:
      * triangulo(8) produce:
@@ -31,19 +33,33 @@ public class Ejercicio2 {
      * @param val numero iteraciones
      */
     public void generarTriangulo(int val){
-        int[] linea = new int[val];
-        for(int i = 0; i < val; i++){
-            linea[i] = 1;
-            for(int j = 0; j < i; j++){
-                linea[j] = linea[j] + linea[j+1];
+        val = val + 1;
+        int[] a = new int[1];
+        for (int i = 1; i <= val; i++) {
+            int[] x = new int[i];
+            // tenemos que llenar de espacios la linea segun el numero de iteraciones
+            // hay que tener en cuenta que el espacio entre numeros es 4
+            int lim = val - i;
+            for (int j = 0; j < lim; j++) {
+                System.out.print(" ");
             }
-            for(int j = 0; j < linea.length; j++){
-                System.out.print(linea[j] + " ");
+            for (int j = 0; j < i; j++) {
+                if (j == 0 || j == (i - 1)) {
+                    x[j] = 1;
+                } else {
+                    x[j] = a[j] + a[j - 1];
+                }
+                if(x[j] < 10){
+                    System.out.print(" ");
+                }
+                System.out.print(x[j] + "    ");
             }
+            a = x;
             System.out.println();
         }
     }
-    
+
+
     public void run() {
         generarTriangulo(8);
     }
